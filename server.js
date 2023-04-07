5eebcf951d3bf3089e1c986e9ffa50b97b6e2125
ualiu <urim.aliu@gmail.com>
2023-04-07 16:34:08 -0400

    added vercel

--- server.js
+++ server.js
@@ -46,5 +46,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+if (process.env.PORT) {
+  app.listen(process.env.PORT)
+}
+
+module.exports = app
